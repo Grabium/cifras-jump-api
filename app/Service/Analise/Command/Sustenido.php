c6b83b6a5ada5bcd1903fb1379d5413be6dfac57
Gabriel Azzevedo <110872805+Grabium@users.noreply.github.com>
2025-06-02 01:02:32 -0300

    hotfix

--- app/Service/Analise/Command/Sustenido.php
+++ app/Service/Analise/Command/Sustenido.php
@@ -6,8 +6,9 @@ class Sustenido extends Command
 {
     public function analisar()
     {
-        if($this->key != 2){
+        if($this->key != 1){
             //processar intervalo
+            echo 'Enttrou no if de intervalo'.PHP_EOL;
         }
 
         //match na classe TomFundamental
